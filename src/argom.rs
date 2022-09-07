663939e1345f6b8ed8f24e00ac7de695ff4f9f9c
Nicola Destro <nicolade03@gmail.com>
2022-09-07 17:03:35 +0200

    Primo commit

---
+++ src/argom.rs
@@ -0,0 +1,13 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short, long, value_parser)]
+    pub url: Option<String>,
+
+    #[clap(short, long, value_parser)]
+    pub path: Option<PathBuf>,
+}
