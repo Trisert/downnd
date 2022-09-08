1f9eca86360c6d9da40da0c769d28ead0c476b4b
Nicola Destro <nicolade03@gmail.com>
2022-09-08 14:45:24 +0200

    Move in lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 pub mod argom;
 use crate::argom::Args;
+use downnd::*;
 
 use anyhow::Result;
 use clap::Parser;
 use futures::stream::StreamExt;
-use reqwest::Response;
-use std::io::{BufRead, Cursor};
-use std::path::Path;
-use tokio::fs::File;
+use std::io::Cursor;
 use tokio::io::copy;
 
 #[tokio::main]
@@ -42,30 +40,4 @@ async fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-async fn create_file(resp: &Response) -> Result<File> {
-    let file = {
-        let fname = resp
-            .url()
-            .path_segments()
-            .and_then(|f| f.last())
-            .and_then(|name| if name.is_empty() { None } else { Some(name) })
-            .unwrap_or("Temp");
-
-        println!("File da scaricare: {fname} ({:.2} MB)", match resp.content_length() {
-            Some(num) => num as f64 / (1024 * 1024) as f64,
-            None => 0 as f64,
-        });
-
-        File::create(fname).await?
-    };
-    Ok(file)
-}
-
-fn read_line(path: &Path) -> Result<Vec<String>> {
-    let file = std::fs::File::open(path)?;
-    let buf = std::io::BufReader::new(file);
-
-    Ok(buf.lines().filter_map(Result::ok).collect())
 }
