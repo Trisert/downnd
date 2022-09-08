8bb7cae4a0df6ed3623355648e87c867e8ac35a6
Nicola Destro <nicolade03@gmail.com>
2022-09-08 12:53:28 +0200

    Larghezza dei file

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,17 @@ use crate::argom::Args;
 
 use anyhow::Result;
 use clap::Parser;
+use futures::stream::StreamExt;
 use reqwest::Response;
-use std::{
-    io::{BufRead, Cursor},
-    path::PathBuf,
-};
-use tokio;
+use std::io::{BufRead, Cursor};
+use std::path::Path;
 use tokio::fs::File;
 use tokio::io::copy;
-use futures::stream::StreamExt;
-use std::path::Path;
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
+
     if let Some(url) = args.url.as_deref() {
         let response = reqwest::get(url).await?;
 
@@ -29,29 +25,20 @@ async fn main() -> Result<()> {
 
     if let Some(path) = args.path.as_deref() {
         let path = read_line(path)?;
-        let fetches = futures::stream::iter(
-            path.into_iter().map(|path| {
-                async move {
-                    match reqwest::get(&path).await {
-                        Ok(resp) => {
-                            let mut dest = create_file(&resp).await.unwrap();
-                            let mut content = Cursor::new(resp.bytes().await.unwrap());
-                            copy(&mut content, &mut dest).await;
-                        },
-                        Err(_) => println!("Error in {}", path),
-                    }
+        let fetches = futures::stream::iter(path.into_iter().map(|path| async move {
+            match reqwest::get(&path).await {
+                Ok(resp) => {
+                    let mut dest = create_file(&resp).await.unwrap();
+                    let mut content = Cursor::new(resp.bytes().await.unwrap());
+                    copy(&mut content, &mut dest).await.unwrap();
                 }
-            })
-        ).buffer_unordered(200).collect::<Vec<()>>();
+                Err(_) => println!("Error in {}", path),
+            }
+        }))
+        .buffer_unordered(100)
+        .collect::<Vec<()>>();
 
         fetches.await;
-        //for i in path.lines() {
-        //    let response = reqwest::get(i.unwrap()).await?;
-
-        //    let mut dest = create_file(&response).await?;
-        //    let mut content = Cursor::new(response.bytes().await?);
-        //    copy(&mut content, &mut dest).await?;
-        //}
     }
 
     Ok(())
@@ -66,7 +53,10 @@ async fn create_file(resp: &Response) -> Result<File> {
             .and_then(|name| if name.is_empty() { None } else { Some(name) })
             .unwrap_or("Temp");
 
-        println!("File da scaricare: {}", fname);
+        println!("File da scaricare: {fname} ({:.2} MB)", match resp.content_length() {
+            Some(num) => num as f64 / (1024 * 1024) as f64,
+            None => 0 as f64,
+        });
 
         File::create(fname).await?
     };
